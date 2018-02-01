Brycey92/BiomeTweaker

commit cd62b0f8d23f82ac9598cd2ba95b78ec8849763a

Accept entity ids as well as the class for adding spawns #106
(cherry picked from commit db6441c06a9ca555f5f0045b44a12e6dd5c32298)

src/main/java/me/superckl/biometweaker/script/command/entity/ScriptCommandAddRemoveSpawn.java
@@ -10,7 +10,10 @@ import me.superckl.api.biometweaker.script.pack.IBiomePackage;
 import me.superckl.api.biometweaker.util.SpawnListType;
 import me.superckl.api.superscript.command.IScriptCommand;
 import me.superckl.biometweaker.config.Config;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityList;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.Biome.SpawnListEntry;
 import net.minecraftforge.common.MinecraftForge;
@@ -34,11 +37,16 @@ public class ScriptCommandAddRemoveSpawn implements IScriptCommand{
 
 	@Override
 	public void perform() throws Exception {
-		Class<? extends EntityLiving> clazz;
+		Class<? extends EntityLiving> clazz = null;
 		try{
 			clazz = (Class<? extends EntityLiving>) Class.forName(this.entityClass);
 		}catch(final Exception e){
-			throw new IllegalArgumentException("Failed to load entity class: "+this.entityClass, e);
+			final Class<? extends Entity> clazz2 = EntityList.getClass(new ResourceLocation(this.entityClass));
+			if(clazz2 == null)
+				throw new IllegalArgumentException("Failed to load entity class: "+this.entityClass, e);
+			if(!EntityLiving.class.isAssignableFrom(clazz2))
+				throw new IllegalArgumentException("entity class "+this.entityClass+" is not assignable to EntityLiving. It cannot be spawned!");
+			clazz = (Class<? extends EntityLiving>) clazz2;
 		}
 		final SpawnListEntry entry = new SpawnListEntry(clazz, this.weight, this.minCount, this.maxCount);
 		final Iterator<Biome> it = this.pack.getIterator();

commit e95f24b2fd4270077ecb6899bd17699c98beee9f

Fixed using entity ids for adding/removing spawns
Fixes compile error introduced by cd62b0f8d23f82ac9598cd2ba95b78ec8849763a

src/main/java/me/superckl/biometweaker/script/command/entity/ScriptCommandAddRemoveSpawn.java
@@ -13,7 +13,6 @@ import me.superckl.biometweaker.config.Config;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityList;
 import net.minecraft.entity.EntityLiving;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.Biome.SpawnListEntry;
 import net.minecraftforge.common.MinecraftForge;
@@ -41,7 +40,8 @@ public class ScriptCommandAddRemoveSpawn implements IScriptCommand{
 		try{
 			clazz = (Class<? extends EntityLiving>) Class.forName(this.entityClass);
 		}catch(final Exception e){
-			final Class<? extends Entity> clazz2 = EntityList.getClass(new ResourceLocation(this.entityClass));
+			//final Class<? extends Entity> clazz2 = EntityList.getClass(new ResourceLocation(this.entityClass));
+			final Class<? extends Entity> clazz2 = EntityList.getClassFromID(EntityList.getIDFromString(entityClass));
 			if(clazz2 == null)
 				throw new IllegalArgumentException("Failed to load entity class: "+this.entityClass, e);
 			if(!EntityLiving.class.isAssignableFrom(clazz2))
@@ -59,6 +59,7 @@ public class ScriptCommandAddRemoveSpawn implements IScriptCommand{
 			this.handleTypeSwitch(gen, entry, clazz);
 			Config.INSTANCE.onTweak(Biome.getIdForBiome(gen));
 		}
+		//LogHelper.info("AAA: "+EntityList.getEntityStringFromClass(clazz));
 	}
 
 	private void handleTypeSwitch(final Biome gen, final SpawnListEntry entry, final Class<?> clazz){